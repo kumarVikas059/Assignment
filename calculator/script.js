d17b799f4dd6425c53f61d04733cff457074dfa1
Vikas Kumar <122825125+kumarVikas059@users.noreply.github.com>
2023-03-17 00:55:11 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,7 +1,7 @@
 function calculate(){
     let n1=parseFloat(document.getElementById("n1").value);
     let n2=parseFloat(document.getElementById("n2").value);
-    let op=document.getElementById("op");
+    let op=document.getElementById("op").value;
     let message=document.getElementById("error");
     switch (op) {
         case '+':
